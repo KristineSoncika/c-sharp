6cc2ba3cb12da98208f56b4cc03f617465685af3
kristine <57680335+kristtiine@users.noreply.github.com>
2022-07-27 16:34:01 +0300

    Exercise 6 done

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise_6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintSum();
+        }
+        static void PrintSum()
+        {
+            Console.WriteLine("Enter a series of single digit numbers without any spaces: ");
+            string numberSeries = Console.ReadLine();
+            double sum = 0;
+            for(int i = 0; i < numberSeries.Length; i++)
+            {
+                sum += Char.GetNumericValue(numberSeries[i]);
+            }
+            Console.WriteLine($"The sum is {sum}.");
+        }
+    }
+}
+
