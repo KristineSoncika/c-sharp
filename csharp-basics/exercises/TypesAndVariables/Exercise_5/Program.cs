b3f49f2d8ce6577a350fc6719df8ae2a12543a57
kristine <57680335+kristtiine@users.noreply.github.com>
2022-07-27 16:06:41 +0300

    Exercise 5 done

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise_5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintCourseSchedule();
+        }
+        static void PrintCourseSchedule()
+        {
+            string tableHorizontalBorder = $"{("+").PadRight(61, '-')}+";
+            string[] courses = { "English III", "Precalculus", "Music Theory", "Biotechnology", "Principles of Technology I", "Latin II", "AP US History", "Business Computer Infomation Systems" };
+            string[] teachers = { "Ms. Lapan", "Mrs. Gideon", "Mr. Davis", "Ms. Palmer", "Ms. Garcia", "Mrs. Barnett", "Ms. Johannessen", "Mr. James" };
+            int orderNumber = 1;
+
+            Console.WriteLine(tableHorizontalBorder);
+            for (int i = 0; i < courses.Length; i++)
+            {
+                Console.WriteLine($"| {orderNumber} | {courses[i],36} | {teachers[i],15} |");
+                orderNumber++;
+            }
+            Console.WriteLine(tableHorizontalBorder);
+        }
+    }
+}
+
