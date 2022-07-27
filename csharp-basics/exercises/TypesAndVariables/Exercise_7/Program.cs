98130c6a464e86d85cf1526b2573a69938b3b11d
kristine <57680335+kristtiine@users.noreply.github.com>
2022-07-27 17:51:55 +0300

    Exercise 7 done

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise_7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountUppercaseLetters();
+        }
+        static void CountUppercaseLetters()
+        {
+            Console.WriteLine("Enter a word or a sentence: ");
+            string sentence = Console.ReadLine();
+            int count = 0;
+            foreach (char letter in sentence)
+            {
+                if (Char.IsUpper(letter))
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"The number of uppercase letters in the sentence: {count}.");
+        }
+    }
+}
+
