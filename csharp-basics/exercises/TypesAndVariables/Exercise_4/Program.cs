5e5cb5d49851923e29660bcecc304017f59046f2
kristine <57680335+kristtiine@users.noreply.github.com>
2022-07-27 13:27:59 +0300

    Exercise_4 done

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise_4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sandy = Bio("Sandy", 1960);
+            Console.WriteLine(sandy);
+        }
+
+        static string Bio(string name, int birthYear)
+        {
+            return $"My name is {name} and I as born in {birthYear}.";
+        }
+    }
+}
