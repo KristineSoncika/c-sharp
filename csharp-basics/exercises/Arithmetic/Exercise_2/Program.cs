bc016e5b9b2e8428924a292c22c6a6a80d2a7cbb
kristine <57680335+kristtiine@users.noreply.github.com>
2022-07-30 10:50:57 +0300

    Exercise 2 done

---
+++ csharp-basics/exercises/Arithmetic/Exercise_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ex_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            CheckOddEven(5);
+        }
+
+        static void CheckOddEven(int num)
+        {
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Even Number");
+            }
+            else
+            {
+                Console.WriteLine("Odd Number");
+            }
+            Console.WriteLine("bye!");
+        }
+    }
+}
+
