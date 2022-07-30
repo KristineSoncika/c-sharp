face3add5262a6a99b03d0c76dbfc34f1d5ff9cf
kristine <57680335+kristtiine@users.noreply.github.com>
2022-07-30 11:13:05 +0300

    Exercise 4 done

---
+++ csharp-basics/exercises/Arithmetic/Exercise_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Product1ToN(4));
+        }
+
+        static string Product1ToN(int num)
+        {
+            int result = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                result *= i;
+            }
+
+            return $"The factorial of {num} is {result}.";
+        }
+    }
+}
+
