25917491db6ec0818979b2e5e3662ea93e97fa8b
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-02 09:54:06 +0300

    PositiveNegativeNumber done

---
+++ csharp-basics/exercises/FlowOfControl/FlowControl/PositiveNegativeNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PositiveNegativeNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            PositiveOrNegative();
+        }
+
+        static void PositiveOrNegative()
+        {
+            Console.WriteLine("Enter a number:");
+            var num = Convert.ToDouble(Console.ReadLine());
+
+            if (num > 0)
+            {
+                Console.WriteLine("Number is positive");
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine("Number is negative");
+            }
+            else
+            {
+                Console.WriteLine("Number is zero");
+            }
+        }
+    }
+}
