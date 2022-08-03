c7551a947ba75033fe87d6fd607e33e05de6c30b
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-03 09:52:29 +0300

    fix else if in Ex 5

--- csharp-basics/exercises/FlowOfControl/Exercise_5/Program.cs
+++ csharp-basics/exercises/FlowOfControl/Exercise_5/Program.cs
@@ -10,66 +10,6 @@ namespace Exercise_5
         }
 
         //Solution 1
-        static string PhoneKeyPad(string text)
-        {
-            text = text.ToLower();
-            string digitSequence = "";
-
-            for (int i = 0; i < text.Length; i++)
-            {
-                switch (text[i])
-                {
-                    case 'a':
-                    case 'b':
-                    case 'c':
-                        digitSequence += '2';
-                        break;
-                    case 'd':
-                    case 'e':
-                    case 'f':
-                        digitSequence += '3';
-                        break;
-                    case 'g':
-                    case 'h':
-                    case 'i':
-                        digitSequence += '4';
-                        break;
-                    case 'j':
-                    case 'k':
-                    case 'l':
-                        digitSequence += '5';
-                        break;
-                    case 'm':
-                    case 'n':
-                    case 'o':
-                        digitSequence += '6';
-                        break;
-                    case 'p':
-                    case 'q':
-                    case 'r':
-                    case 's':
-                        digitSequence += '7';
-                        break;
-                    case 't':
-                    case 'u':
-                    case 'v':
-                        digitSequence += '8';
-                        break;
-                    case 'w':
-                    case 'x':
-                    case 'y':
-                    case 'z':
-                        digitSequence += '9';
-                        break;
-                    default:
-                        return "Invalid input text";
-                }
-            }
-
-            return digitSequence;
-        }
-
-        //Solution 2
         //static string PhoneKeyPad(string text)
         //{
         //    text = text.ToLower();
@@ -77,48 +17,108 @@ namespace Exercise_5
 
         //    for (int i = 0; i < text.Length; i++)
         //    {
-        //        if (!Char.IsLetter(text[i]))
+        //        switch (text[i])
         //        {
-        //            return "Invalid input text";
-        //        }
-        //        else
-        //        {
-        //            if (text[i] == 'a' || text[i] == 'b' || text[i] == 'c')
-        //            {
+        //            case 'a':
+        //            case 'b':
+        //            case 'c':
         //                digitSequence += '2';
-        //            }
-        //            else if (text[i] == 'd' || text[i] == 'e' || text[i] == 'f')
-        //            {
+        //                break;
+        //            case 'd':
+        //            case 'e':
+        //            case 'f':
         //                digitSequence += '3';
-        //            }
-        //            if (text[i] == 'g' || text[i] == 'h' || text[i] == 'i')
-        //            {
+        //                break;
+        //            case 'g':
+        //            case 'h':
+        //            case 'i':
         //                digitSequence += '4';
-        //            }
-        //            if (text[i] == 'j' || text[i] == 'k' || text[i] == 'l')
-        //            {
+        //                break;
+        //            case 'j':
+        //            case 'k':
+        //            case 'l':
         //                digitSequence += '5';
-        //            }
-        //            if (text[i] == 'm' || text[i] == 'n' || text[i] == 'o')
-        //            {
+        //                break;
+        //            case 'm':
+        //            case 'n':
+        //            case 'o':
         //                digitSequence += '6';
-        //            }
-        //            if (text[i] == 'p' || text[i] == 'q' || text[i] == 'r' || text[i] == 's')
-        //            {
+        //                break;
+        //            case 'p':
+        //            case 'q':
+        //            case 'r':
+        //            case 's':
         //                digitSequence += '7';
-        //            }
-        //            if (text[i] == 't' || text[i] == 'u' || text[i] == 'v')
-        //            {
+        //                break;
+        //            case 't':
+        //            case 'u':
+        //            case 'v':
         //                digitSequence += '8';
-        //            }
-        //            if (text[i] == 'w' || text[i] == 'x' || text[i] == 'y' || text[i] == 'z')
-        //            {
+        //                break;
+        //            case 'w':
+        //            case 'x':
+        //            case 'y':
+        //            case 'z':
         //                digitSequence += '9';
-        //            }
+        //                break;
+        //            default:
+        //                return "Invalid input text";
         //        }
         //    }
 
         //    return digitSequence;
         //}
+
+        //Solution 2
+        static string PhoneKeyPad(string text)
+        {
+            text = text.ToLower();
+            string digitSequence = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!Char.IsLetter(text[i]))
+                {
+                    return "Invalid input text";
+                }
+                else
+                {
+                    if (text[i] == 'a' || text[i] == 'b' || text[i] == 'c')
+                    {
+                        digitSequence += '2';
+                    }
+                    else if (text[i] == 'd' || text[i] == 'e' || text[i] == 'f')
+                    {
+                        digitSequence += '3';
+                    }
+                    else if (text[i] == 'g' || text[i] == 'h' || text[i] == 'i')
+                    {
+                        digitSequence += '4';
+                    }
+                    else if (text[i] == 'j' || text[i] == 'k' || text[i] == 'l')
+                    {
+                        digitSequence += '5';
+                    }
+                    else if (text[i] == 'm' || text[i] == 'n' || text[i] == 'o')
+                    {
+                        digitSequence += '6';
+                    }
+                    else if (text[i] == 'p' || text[i] == 'q' || text[i] == 'r' || text[i] == 's')
+                    {
+                        digitSequence += '7';
+                    }
+                    else if (text[i] == 't' || text[i] == 'u' || text[i] == 'v')
+                    {
+                        digitSequence += '8';
+                    }
+                    else if (text[i] == 'w' || text[i] == 'x' || text[i] == 'y' || text[i] == 'z')
+                    {
+                        digitSequence += '9';
+                    }
+                }
+            }
+
+            return digitSequence;
+        }
     }
 }
