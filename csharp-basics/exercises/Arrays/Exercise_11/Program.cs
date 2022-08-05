55cb0f747c80c87ff385e97ab435be127cf66407
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-05 10:10:33 +0300

    Ex 11 done

---
+++ csharp-basics/exercises/Arrays/Exercise_11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercise_11
+{
+    class Program
+    {
+        private static void Main()
+        {
+            FindNemo("I am finding Nemo !");
+            FindNemo("Nemo is me");
+            FindNemo("I Nemo am");
+            FindNemo("Nemo's cat is black");
+        }
+
+        static void FindNemo(string sentence)
+        {
+            string[] wordArr = sentence.Split(" ");
+            int wordIndex = Array.IndexOf(wordArr, "Nemo");
+            int wordOrder = wordIndex + 1;
+
+            if (wordIndex == -1)
+            {
+                Console.WriteLine("I can't find Nemo :(");
+            }
+            else
+            {
+                Console.WriteLine($"I found Nemo at {wordOrder}!");
+            }
+        }
+    }
+}
