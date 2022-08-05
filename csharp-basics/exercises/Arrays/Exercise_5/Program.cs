49ab4ac3ce03ddb1be5c40d732734411b6165e14
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-05 09:42:27 +0300

    Ex 5 done

---
+++ csharp-basics/exercises/Arrays/Exercise_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise_5
+{
+    class Program
+    {
+        private static void Main()
+        {
+            int[] numberArray = { 25, 14, 56, 15, 36, 56, 77, 18, 29, 49 };
+            FindIndex(numberArray, 36);
+            FindIndex(numberArray, 29);
+        }
+
+        static void FindIndex(int[] numArr, int num)
+        {
+            int index = Array.IndexOf(numArr, num);
+            Console.WriteLine($"Index position of {num} is: {index}");
+        }
+    }
+}
