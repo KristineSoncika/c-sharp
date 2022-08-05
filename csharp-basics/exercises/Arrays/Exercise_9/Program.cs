b7ee48fbaf634c27a9b9f3ab9b16c05e6a94e812
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-05 10:06:50 +0300

    Ex 9 done

---
+++ csharp-basics/exercises/Arrays/Exercise_9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercise_9
+{
+    class Program
+    {
+        private static void Main()
+        {
+            string[] arr1 = { "mavis", "senaida", "letty" };
+            string[] arr2 = { "samuel", "MABELLE", "letitia", "meridith" };
+            string[] arr3 = { "Sylvia", "Kristal", "Sharilyn", "Calista" };
+            Capitalize(arr1);
+            Capitalize(arr2);
+            Capitalize(arr3);
+        }
+
+        static string[] Capitalize(string[] arr)
+        {
+            string[] capitalizedArr = new string[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                string formattedWord = arr[i].ToLower();
+                formattedWord = Char.ToUpper(formattedWord[0]) + formattedWord[1..];
+                capitalizedArr[i] = formattedWord;
+            }
+
+            Console.WriteLine(String.Join(", ", capitalizedArr));
+            return capitalizedArr;
+        }
+    }
+}
