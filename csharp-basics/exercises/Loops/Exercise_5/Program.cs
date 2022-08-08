d89f7767a429c0006edf64f44b6880ae7b2fd7d2
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-08 12:35:41 +0300

    Ex 5 done

---
+++ csharp-basics/exercises/Loops/Exercise_5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise_5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the first word:");
+            string firstWord = Console.ReadLine();
+            Console.WriteLine("Enter the second word:");
+            string secondWord = Console.ReadLine();
+
+            int lineLength = 30;
+            int dotCount = lineLength - firstWord.Length - secondWord.Length;
+
+            Console.Write(firstWord);
+
+            for (int i = 0; i < dotCount; i++)
+            {
+                Console.Write(".");
+            }
+
+            Console.Write(secondWord);
+        }
+    }
+}
