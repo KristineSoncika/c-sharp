d8c296f467aeeddd631924c5e6058d48869c14bd
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-08 12:18:13 +0300

    Ex 4 done

---
+++ csharp-basics/exercises/Loops/Exercise_4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+            for(int i = 0; i < vowels.Length; i++)
+            {
+                Console.WriteLine(vowels[i]);
+            }
+
+            foreach(var i in vowels)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
