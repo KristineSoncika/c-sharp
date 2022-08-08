671832612e2bd7d7e819a085b236620e252e522c
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-08 18:00:26 +0300

    fix ex 6

--- csharp-basics/exercises/Loops/Exercise_6/Program.cs
+++ csharp-basics/exercises/Loops/Exercise_6/Program.cs
@@ -6,36 +6,8 @@ namespace Exercise_6
     {
         static void Main()
         {
-            FizzBuzz(100);
-        }
-
-        static void FizzBuzz(int num)
-        {
-            string fizzbuzz = "";
-
-            for (int i = 1; i <= num; i++)
-            {
-                if (!(i % 3 == 0) && !(i % 5 == 0))
-                {
-                    Console.Write($"{i} ");                    
-                }
-                if (i % 3 == 0)
-                {
-                    fizzbuzz += "Fizz";
-                }
-                if (i % 5 == 0)
-                {
-                    fizzbuzz += "Bazz";
-                }
-
-                Console.Write($"{fizzbuzz} ");
-                fizzbuzz = "";
-
-                if (i % 20 == 0)
-                {
-                    Console.Write(Environment.NewLine);
-                }
-            }
-        }
+            FizzBuzz fizzBuzz = new();
+            fizzBuzz.Game();
+        }        
     }
 }
