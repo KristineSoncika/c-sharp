28921742295db6a21cce7558823c19632d940ef7
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-08 20:03:37 +0300

    Ex 7 done

---
+++ csharp-basics/exercises/Loops/Exercise_7/Piglet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercise_7
+{
+    public class Piglet
+    {
+        public void Game()
+        {
+            Random randomNum = new();
+            int dice = randomNum.Next(1, 7);
+            int total = 0;
+            string reply = "yes";
+
+            Console.WriteLine("Welcome to Piglet!");
+
+            while (reply == "yes")
+            {
+                if (dice == 1)
+                {
+                    total = 0;
+                    Console.WriteLine($"You rolled {dice}");
+                    Console.WriteLine($"Game over!. You got {total} points.");
+                    Environment.Exit(0);
+                }
+
+                Console.WriteLine($"You rolled {dice}");
+                total += dice;
+                Console.WriteLine($"Roll again? Type YES or NO");
+                reply = Console.ReadLine().ToLower();
+                dice = randomNum.Next(1, 7);  
+            }
+
+            Console.WriteLine($"Game over!. You got {total} points.");
+        }
+    }
+}
