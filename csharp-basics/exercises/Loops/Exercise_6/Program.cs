d97ad2dee34a0c9802e3914012ba6fd9472340e4
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-08 14:40:49 +0300

    Ex 6 done

---
+++ csharp-basics/exercises/Loops/Exercise_6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercise_6
+{
+    class Program
+    {
+        static void Main()
+        {
+            FizzBuzz(100);
+        }
+
+        static void FizzBuzz(int num)
+        {
+            string fizzbuzz = "";
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (!(i % 3 == 0) && !(i % 5 == 0))
+                {
+                    Console.Write($"{i} ");                    
+                }
+                if (i % 3 == 0)
+                {
+                    fizzbuzz += "Fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    fizzbuzz += "Bazz";
+                }
+
+                Console.Write($"{fizzbuzz} ");
+                fizzbuzz = "";
+
+                if (i % 20 == 0)
+                {
+                    Console.Write(Environment.NewLine);
+                }
+            }
+        }
+    }
+}
