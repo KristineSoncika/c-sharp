4e69010d328ca1b6aff20bfe5b1c9e7b8bc97b9e
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-09 11:07:47 +0300

    Ex 10 done

---
+++ csharp-basics/exercises/Loops/Exercise_10/NumberSquare.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise_10
+{
+    public class NumberSquare
+    {
+        public void GenerateSquare(int min, int max)
+        {
+            List<int> numArr = new();
+
+            for (int i = min; i <= max; i++)
+            {
+                numArr.Add(i);
+            }
+
+            int index = 0;
+
+            for (int i = 0; i < numArr.Count; i++)
+            {                
+                for (int j = index; j < numArr.Count + index; j++)
+                {
+                    Console.Write(numArr[j % numArr.Count]);                    
+                }
+
+                Console.Write(Environment.NewLine);
+                index++;
+            }
+        }
+    }
+}
---
+++ csharp-basics/exercises/Loops/Exercise_10/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise_10
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter min: ");
+            int min = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter max: ");
+            int max = int.Parse(Console.ReadLine());
+
+            NumberSquare numberSquare = new();
+            numberSquare.GenerateSquare(min, max);
+        }
+    }
+}
