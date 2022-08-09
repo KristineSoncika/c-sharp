8bbe087ab4851b4527acb1c48343bdd667cee61f
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-09 08:46:47 +0300

    Ex 8 done

---
+++ csharp-basics/exercises/Loops/Exercise_8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercise_8
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a number which will determine the size of the figure: ");
+            int lineNumbers = int.Parse(Console.ReadLine());
+
+            AsciiFigure asciiFigure = new();
+            asciiFigure.DrawFigure(lineNumbers);
+        }
+    }
+}
