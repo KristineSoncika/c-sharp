faba5729f8cb673a089b08b53ddc94d4fb5153a0
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-09 09:15:30 +0300

    Ex 9 done

---
+++ csharp-basics/exercises/Loops/Exercise_9/RollTwoDice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise_9
+{
+    public class RollTwoDice
+    {
+        public void Game(int sum)
+        {
+            Random randomNumber = new();
+            int calcSum = 0;
+
+            while(calcSum != sum)
+            {
+                int num1 = randomNumber.Next(1, 7);
+                int num2 = randomNumber.Next(1, 7);
+                calcSum = num1 + num2;
+                Console.WriteLine($"{num1} and {num2} = {calcSum}");
+            }
+        }
+    }
+}
+
