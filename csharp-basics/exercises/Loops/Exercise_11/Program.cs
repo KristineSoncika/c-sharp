29393148593402efd261a93e428345fd1a5281f9
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-09 11:48:34 +0300

    Ex 11 done

---
+++ csharp-basics/exercises/Loops/Exercise_11/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercise_11
+{
+    class Program
+    {
+        static void Main()
+        {
+            ReverseCase("Happy Birthday");
+            ReverseCase("MANY THANKS");
+            ReverseCase("sPoNtAnEoUs");
+        }
+
+        static void ReverseCase(string text)
+        {
+            string reversedText = "";
+
+            foreach(var i in text)
+            {
+                if (Char.IsUpper(i))
+                {
+                    reversedText += Char.ToLower(i);
+                }
+                else if (Char.IsLower(i))
+                {
+                    reversedText += Char.ToUpper(i);
+                }
+                else
+                {
+                    reversedText += i;
+                }
+            }
+
+            Console.WriteLine(reversedText);
+        }
+    }
+}
