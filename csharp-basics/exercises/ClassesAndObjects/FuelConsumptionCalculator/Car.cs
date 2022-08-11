e91990304505e6528dc67add35ea4a8a431d6d92
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-11 10:00:04 +0300

    Fuel Consumption Calc done

---
+++ csharp-basics/exercises/ClassesAndObjects/FuelConsumptionCalculator/Car.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace FuelConsumptionCalculator
+{
+    public class Car
+    {
+        private double _firstReading;
+        private double _currentReading;
+        private double _litersFilled;
+
+        public Car(double firstReading)
+        {
+            _firstReading = firstReading;
+        }
+
+        public double ConsumptionPerKm()
+        {
+            return _litersFilled / (_currentReading - _firstReading);
+        }
+
+        public double ConsumptionPer100Km()
+        {
+            return ConsumptionPerKm() * 100;
+        }
+
+        public bool IsGasHog()
+        {
+            if (ConsumptionPer100Km() > 15)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsEconomyCar()
+        {
+            if (ConsumptionPer100Km() < 5)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void FillUp(double km, double liters)
+        {
+            _currentReading = km;
+            _litersFilled += liters;
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/FuelConsumptionCalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FuelConsumptionCalculator
+{
+    class Program
+    {
+        private static void Main()
+        {
+            Car car1 = new(540);
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.Write("Enter the current reading: ");
+                double currentReading = double.Parse(Console.ReadLine());
+                Console.Write("Enter liters reading: ");
+                double litersFilled = double.Parse(Console.ReadLine());
+                car1.FillUp(currentReading, litersFilled);
+            }
+
+            Console.WriteLine($"L/100km: {car1.ConsumptionPer100Km():F2}. The car is a gas hog: {car1.IsGasHog()}");
+            Console.WriteLine($"L/100km: {car1.ConsumptionPer100Km():F2}. This is an economy car: {car1.IsEconomyCar()}");
+        }
+    }
+}
