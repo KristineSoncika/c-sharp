f50efeef24486447944a3b0367342e5c92d0fad8
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-11 10:19:52 +0300

    Ex 7 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise_7/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise_7
+{
+    class Program
+    {
+        private static void Main()
+        {
+            Console.Write("What is the current balance of your account: ");
+            decimal startingBalance = decimal.Parse(Console.ReadLine());
+            SavingsAccount savingsAccount1 = new(startingBalance);
+
+            Console.Write("Enter the annual interest rate: ");
+            decimal annualInterest = decimal.Parse(Console.ReadLine());
+
+            Console.Write("How long has the account been opened (in months): ");
+            int months = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= months; i++)
+            {
+                Console.Write($"Enter the amount deposited in month {i}: ");
+                decimal deposit = decimal.Parse(Console.ReadLine());
+                savingsAccount1.AddDeposit(deposit);
+
+                Console.Write($"Enter the amount withdrawn in month {i}: ");
+                decimal withdrawal = decimal.Parse(Console.ReadLine());
+                savingsAccount1.AddWithdrawal(withdrawal);
+
+                savingsAccount1.AddMonthlyInterest(annualInterest);
+            }
+
+            CultureInfo culture = CultureInfo.CreateSpecificCulture("en-US");
+
+            string totalDeposited = savingsAccount1.GetTotalDeposited().ToString("C2", culture);
+            string totalWithdrawn = savingsAccount1.GetTotalWithdrawn().ToString("C2", culture);
+            string interestEarned = savingsAccount1.GetInterestEarned().ToString("C2", culture);
+            string balance = savingsAccount1.GetBalance().ToString("C2", culture);
+
+            Console.WriteLine($"Total deposited: {totalDeposited}");
+            Console.WriteLine($"Total withdrawn: {totalWithdrawn}");
+            Console.WriteLine($"Interest earned: {interestEarned}");
+            Console.WriteLine($"Ending balance: {balance}");
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise_7/SavingsAccount.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Exercise_7
+{
+    public class SavingsAccount
+    {
+        private decimal _balance;
+        private decimal _withdrawn;
+        private decimal _deposited;
+        private decimal _interestEarned;
+
+        public SavingsAccount(decimal balance)
+        {
+            _balance = balance;
+        }
+
+        public decimal GetBalance()
+        {
+            return _balance;
+        }
+
+        public decimal GetTotalWithdrawn()
+        {
+            return _withdrawn;
+        }
+
+        public decimal GetTotalDeposited()
+        {
+            return _deposited;
+        }
+
+        public decimal GetInterestEarned()
+        {
+            return _interestEarned;
+        }
+
+        public void AddWithdrawal(decimal withdrawal)
+        {
+            _balance -= withdrawal;
+            _withdrawn += withdrawal;
+        }
+
+        public void AddDeposit(decimal deposit)
+        {
+            _balance += deposit;
+            _deposited += deposit;
+        }
+
+        public void AddMonthlyInterest(decimal annualInterest)
+        {
+            decimal monthlyInterest = annualInterest / 12;
+            decimal monthlyInterestEarned = _balance * monthlyInterest;
+            _interestEarned += monthlyInterestEarned;
+            _balance += monthlyInterestEarned;
+        }
+    }
+}
