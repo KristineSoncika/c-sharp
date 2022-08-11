af560dd07856908fe5180e2bc96e2794ca4404be
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-11 10:13:38 +0300

    EnergyDrinks done

---
+++ csharp-basics/exercises/ClassesAndObjects/EnergyDrinks/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EnergyDrinks
+{
+    class Program
+    {
+        private const int customersSurveyed = 12467;
+        private const double purchasedEnergyDrinks = 0.14;
+        private const double preferCitrusDrinks = 0.64;
+
+        private static void Main()
+        {
+            Console.WriteLine($"Total number of people surveyed: {customersSurveyed}");
+            Console.WriteLine($"Approximately {CustomersBuyingEnergyDrinks(customersSurveyed)} customers bought at least one energy drink");
+            Console.WriteLine($"Around {CustomersPreferingCitrusDrinks(customersSurveyed)} of them prefer citrus-flavored energy drinks");
+        }
+
+        private static double CustomersBuyingEnergyDrinks(int customersSurvayed)
+        {
+            return Math.Floor(purchasedEnergyDrinks * customersSurvayed);
+        }
+
+        private static double CustomersPreferingCitrusDrinks(int customersSurvayed)
+        {
+            return Math.Floor(preferCitrusDrinks * CustomersBuyingEnergyDrinks(customersSurvayed));
+        }
+    }
+}
