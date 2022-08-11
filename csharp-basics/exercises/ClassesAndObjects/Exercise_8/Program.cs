d7dde00724833b5f5d283128684e91ae6ca9deb3
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-11 14:45:07 +0300

    Ex 8 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise_8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+
+namespace Exercise_8
+{
+    class Program
+    {
+        private static void Main()
+        {
+            Point p1 = new(5, 2);
+            Point p2 = new(-3, 6);
+
+            SwapPoints(p1, p2);
+            Console.WriteLine($"({p1.X}, {p1.Y})");
+            Console.WriteLine($"({p2.X}, {p2.Y})");
+        }
+
+        static void SwapPoints(Point point1, Point point2)
+        {
+            int temporaryX = point1.X;
+            int temporaryY = point1.Y;
+            point1.X = point2.X;
+            point1.Y = point2.Y;
+            point2.X = temporaryX;
+            point2.Y = temporaryY;
+        }
+    }
+}
