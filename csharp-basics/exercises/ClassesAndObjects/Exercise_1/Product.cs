c9791105b1b7135718148c38f7e84d82beacd67a
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-11 09:56:37 +0300

    Ex 1 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise_1/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    public class Product
+    {
+        private string _name;
+        private double _price;
+        private int _amount;
+
+        public Product(string name, double priceAtStart, int amountAtStart)
+        {
+            _name = name;
+            _price = priceAtStart;
+            _amount = amountAtStart;
+        }
+
+        public void ChangePrice(double newPrice)
+        {
+            _price = newPrice;
+        }
+
+        public void ChangeAmount(int newAmount)
+        {
+            _amount = newAmount;
+        }
+
+        public void PrintProduct()
+        {
+            Console.WriteLine($"{_name}, price {_price}, amount {_amount}");
+        }
+    }
+}
+
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Product product1 = new("Logitech mouse", 70.00, 14);
+            Product product2 = new("iPhone 5s", 999.99, 3);
+            Product product3 = new("Epson EB-U05", 440.46, 1);
+
+            product1.PrintProduct();
+            product2.PrintProduct();
+            product3.PrintProduct();
+
+            product1.ChangeAmount(15);
+            product1.PrintProduct();
+
+            product3.ChangePrice(556.34);
+            product3.PrintProduct();
+        }
+    }
+}
