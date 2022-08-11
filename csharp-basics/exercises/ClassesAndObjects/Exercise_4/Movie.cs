b0cb06245ea3a95e94160a5729f645b663386ee4
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-11 10:10:24 +0300

    Ex 4 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise_4/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercise_4
+{
+    public class Movie
+    {
+        public string Title;
+        public string Studio;
+        public string Rating;
+
+        public Movie(string title, string studio, string rating)
+        {
+            Title = title;
+            Studio = studio;
+            Rating = rating;
+        }
+
+        public Movie(string title, string studio)
+        {
+            Title = title;
+            Studio = studio;
+            Rating = "PG";
+        }
+
+        public Movie[] GetPG(Movie[] movieArray)
+        {
+            Movie[] ratingPG = Array.FindAll(movieArray, movie => movie.Rating == "PG");
+            return ratingPG;
+        }
+    }
+}
