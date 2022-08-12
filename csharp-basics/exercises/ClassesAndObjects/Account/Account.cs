7de8d6bd4f4514454d0169d8595f077a51a1f97c
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-12 10:10:38 +0300

    Account done

---
+++ csharp-basics/exercises/ClassesAndObjects/Account/Account.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Account
+{
+    class Account
+    {
+        private string _name;
+        private decimal _balance;
+
+        public Account(string name, decimal balance)
+        {
+            _name = name;
+            _balance = balance;
+        }
+
+        public void Withdrawal(decimal withdrawn)
+        {
+            _balance -= withdrawn;
+        }
+
+        public void Deposit(decimal deposited)
+        {
+            _balance += deposited;
+        }
+
+        public decimal Balance()
+        {
+            return _balance;
+        }
+
+        public string Name => _name;
+
+        public override string ToString()
+        {
+            return $"{Name}: {_balance:F2}";
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Account/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Account
+{
+    class VideoStoretest
+    {
+        private static void Main()
+        {
+            Account a = new("A", 100);
+            Account b = new("B", 0);
+            Account c = new("C", 0);
+
+            Transfer(a, b, 50);
+            Transfer(b, c, 25);
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+
+        public static void Transfer(Account from, Account to, decimal amount)
+        {
+            from.Withdrawal(amount);
+            to.Deposit(amount);
+        }
+    }
+}
