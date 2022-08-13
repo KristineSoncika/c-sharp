8b6ca575ad7387bdd65edab5f35fe41609ec6bac
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-13 09:20:28 +0300

    Ex 14 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise_14/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercise_14
+{
+    class Program
+    {
+        private static void Main()
+        {
+            Date date = new();
+            date.WeekdayInDutch(1970, 9, 21);
+            date.WeekdayInDutch(1945, 9, 2);
+            date.WeekdayInDutch(2001, 9, 11);
+        }
+    }
+}
