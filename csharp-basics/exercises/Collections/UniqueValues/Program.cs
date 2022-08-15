60858a1ca08dc2c8762f6ad7d00253ce64718441
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-15 16:32:40 +0300

    UniqueValues done

---
+++ csharp-basics/exercises/Collections/UniqueValues/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueValues
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var values = new List<string> { "Hi", "Meow", "Hello", "Meow", "Hi!", "Meow", "Hi", "Bye" };
+
+            var uniqueValues = values
+                .GroupBy(value => value)
+                .Where(group => group.Count() == 1)
+                .Select(unique => unique.Key);
+
+            Console.WriteLine(String.Join(", ", uniqueValues));
+        }
+    }
+}
