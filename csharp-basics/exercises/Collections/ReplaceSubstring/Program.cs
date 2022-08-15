0c3ade2a056e0fbebf02e3aff7a4caa24ce17d88
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-15 14:40:38 +0300

    ReplaceSubstring done

---
+++ csharp-basics/exercises/Collections/ReplaceSubstring/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ReplaceSubstring
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var words = new[] { "near", "speak", "tonight", "weapon", "customer", "deal", "lawyer" };
+
+            var modifiedWords = words.Select(word => word.Replace("ea", "*"));
+            Console.WriteLine(String.Join(", ", modifiedWords));
+        }
+    }
+}
