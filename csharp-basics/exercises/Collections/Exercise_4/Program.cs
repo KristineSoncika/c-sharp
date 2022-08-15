14f4b29d7ba9cdf9eeb990bf3f0bc779508435e1
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-15 17:16:21 +0300

    Ex 4 done

---
+++ csharp-basics/exercises/Collections/Exercise_4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Exercise_4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var nameList = new List<string>();
+            string name = " ";
+
+            while (name.Length != 0)
+            {
+                Console.Write("Enter a name: ");
+                name = Console.ReadLine();
+                nameList.Add(name);
+            }
+
+            var distinctNameList = nameList.Distinct();
+
+            Console.WriteLine(String.Join(" ", distinctNameList));
+        }
+    }
+}
