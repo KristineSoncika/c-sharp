5d0bf67ec73065ccdff168fb75e1f30abb9ee7f1
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-15 09:00:22 +0300

    List Ex 11 done

---
+++ csharp-basics/exercises/Collections/ListExercise_11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExercise_11
+{
+    class Program
+    {
+        static void Main()
+        {
+            var animals = new List<string>();
+
+            animals.AddRange(new string[] { "zebra", "penguin", "elephant", "lion", "alligator", "snake", "pelican", "tiger", "pigeon", "badger" });
+
+            animals.Insert(4, "ferret");
+
+            animals[animals.Count - 1] = "whale";
+
+            animals.Sort();
+
+            Console.WriteLine($"The animals list contains 'Foobar': {animals.Contains("Foobar")}\n");
+
+            foreach (var animal in animals)
+            {
+                Console.Write($"{animal} ");
+            }
+        }
+    }
+}
