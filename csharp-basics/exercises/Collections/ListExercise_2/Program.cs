beaf69ca3fe2f8c2d451079d5eddd9b3e977aacb
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-15 08:50:18 +0300

    List Ex 2 done

---
+++ csharp-basics/exercises/Collections/ListExercise_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExercise_2
+{
+    class Program
+    {
+        private static void Main()
+        {
+            var colors = new List<string>();
+
+            colors.Add("Red");
+            colors.Add("Green");
+            colors.Add("Orange");
+            colors.Add("White");
+            colors.Add("Black");
+
+            foreach (var color in colors)
+            {
+                Console.WriteLine(color);
+            }
+        }
+    }
+}
