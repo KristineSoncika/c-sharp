39398c163e749cda51cdd102f25732bfe92ea4a7
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-16 00:11:51 +0300

    WordCount done

---
+++ csharp-basics/exercises/Collections/WordCount/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+using System.Numerics;
+
+namespace WordCount
+{
+    class Program
+    {
+        static void Main()
+        {
+            WordCount();
+        }
+
+        static void WordCount()
+        {
+            var text = File.ReadAllText(Directory.GetCurrentDirectory() + @"/lear.txt");
+
+            int lines = text.Split("\n").Count();
+            int chars = text.Count() - text.Count(i => i == '\n');
+
+            text = text.Replace("  ", " ").Replace("\n", " ");
+            string[] stringSeparators = new string[] { " ", "'" };
+            int words = text.Split(stringSeparators, StringSplitOptions.None).Count();
+
+            Console.WriteLine($"Lines = {lines}");
+            Console.WriteLine($"Words = {words}");
+            Console.WriteLine($"Chars = {chars}");
+        }
+    }
+}
