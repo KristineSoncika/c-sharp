ff8d6294f50ad6566d41425c65197bebd6268509
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-15 09:24:21 +0300

    DayNames done

---
+++ csharp-basics/exercises/Collections/DayNames/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayNames
+{
+    class Program
+    {
+        static void Main()
+        {
+            DayName(2021, 10, 18);
+        }
+
+        static void DayName(int year, int month, int day)
+        {
+            DateTime date = new DateTime(year, month, day);
+            Console.Write($"The day of the week for {date:d} is {date.DayOfWeek}");
+        }
+    }
+}
