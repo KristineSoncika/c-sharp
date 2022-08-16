d7803b30ce95bd96ec8e65e8fb461942149c81fe
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-16 22:56:07 +0300

    Ex 10 done

---
+++ csharp-basics/exercises/Collections/Exercise_10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Phonebook
+{
+    class Program
+    {
+        static void Main()
+        {
+            HashSet<string> fruits = new();
+            fruits.Add("Lemon");
+            fruits.Add("Apple");
+            fruits.Add("Orange");
+            fruits.Add("Plum");
+            fruits.Add("Pear");
+
+            Console.WriteLine(String.Join(" | ", fruits));
+
+            fruits.Clear();
+
+            Console.WriteLine(String.Join(" | ", fruits));
+
+            fruits.Add("Lemon");
+            fruits.Add("Lemon");
+
+            Console.WriteLine(String.Join(" | ", fruits));
+        }
+    }
+}
