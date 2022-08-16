19a096a89e34be9a264e46f9e7e78a61eb343314
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-16 22:29:33 +0300

    PhoneBook done

---
+++ csharp-basics/exercises/Collections/Phonebook/PhoneDirectory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook
+{
+    public class PhoneDirectory
+    {
+        private SortedList<string, string> _directory;
+
+        public PhoneDirectory()
+        {
+            _directory = new();
+        }
+
+        public string GetNumber(string name)
+        {
+            return _directory.ContainsKey(name) ? _directory[name] : "Number not found!";
+        }
+
+        public void AddNumber(string name, string number)
+        {
+            if (name == null || number == null)
+            {
+                throw new Exception("name and number cannot be null");
+            }
+
+            _directory.Add(name, number);
+        }
+    }
+}
---
+++ csharp-basics/exercises/Collections/Phonebook/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook
+{
+    class Program
+    {
+        static void Main()
+        {
+            PhoneDirectory directory = new();
+            Console.WriteLine(directory.GetNumber("Barry"));
+            directory.AddNumber("Barry", "20217654");
+            Console.WriteLine(directory.GetNumber("Barry"));
+        }
+    }
+}
