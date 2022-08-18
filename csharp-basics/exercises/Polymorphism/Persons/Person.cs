e25ef77109f4940577be416ad0c56d7422379e62
kristine <57680335+kristtiine@users.noreply.github.com>
2022-08-18 12:45:53 +0300

    Persons done

---
+++ csharp-basics/exercises/Polymorphism/Persons/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Persons
+{
+    public class Person
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _address;
+        private int _id;
+
+        public Person(string firstName, string lastName, string address, int id)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _address = address;
+            _id = id;
+        }
+
+        public string GetFirstName()
+        {
+            return _firstName;
+        }
+
+        public string GetLastName()
+        {
+            return _lastName;
+        }
+
+        public string GetAddress()
+        {
+            return _address;
+        }
+
+        public int GetId()
+        {
+            return _id;
+        }
+
+        public virtual void Display()
+        {
+            Console.WriteLine($"Name: {GetFirstName()} {GetLastName()}");
+            Console.WriteLine($"Address: {GetAddress()}");
+            Console.WriteLine($"ID: {GetId()}");
+        }
+    }
+}
---
+++ csharp-basics/exercises/Polymorphism/Persons/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Persons
+{
+    class Program
+    {
+        static void Main()
+        {
+            Student student = new("Barry", "Blue", "7036 Mulholland Drive, Los Angeles, CA 90068, USA", 345987465, 3.2);
+            Employee employee = new("Sandy", "Mess", "101  Eagle, Fargo, ND 58102, USA", 786543098, "CTO");
+
+            student.Display();
+            student.YearEndCheck();
+            student.Display();
+
+            employee.Display();
+        }
+    }
+}
